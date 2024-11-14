c465f0a958bc1d86ebb956ebad4dc2f63e6b22a4
Srijak Chakraborty <srijak.work@gmail.com>
2024-11-14 22:18:26 +0530

    Spring application creation

---
+++ src/main/java/srijakchakraborty/io/invoicemanager/InvoicemanagerApplication.java
@@ -0,0 +1,13 @@
+package srijakchakraborty.io.invoicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/srijakchakraborty/io/invoicemanager/InvoicemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package srijakchakraborty.io.invoicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
